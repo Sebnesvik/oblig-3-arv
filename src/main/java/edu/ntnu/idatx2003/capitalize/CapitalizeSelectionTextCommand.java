fdfe4cb1d66b8d673e29d3a5b82f9b96adac1014
Sebastian <sebastian.lid.nesvik@gmail.com>
2025-02-19 15:29:00 +0100

    Added getSelection

--- src/main/java/edu/ntnu/idatx2003/capitalize/CapitalizeSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idatx2003/capitalize/CapitalizeSelectionTextCommand.java
@@ -7,6 +7,11 @@ public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand{
     this.selection = selection;
   }
 
+  public String getSelection() {
+    return selection;
+
+  }
+
   @Override
   public String execute(String text) {
     if (text == null || selection == null || text.isEmpty()) {
